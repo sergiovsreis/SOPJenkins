fe37c5d3765d32a814969b2b894b99a986b3dfae
fhict <fhict>
2017-03-23 14:02:40 +0100

    user and usertest added

---
+++ SOPJenkins/src/test/java/domain/UserTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author fhict
+ */
+public class UserTest {
+    
+    public UserTest() {
+    }
+
+    @Test
+    public void createUser() {
+        User tempUser = new User("Sergio");
+        
+       assertEquals(tempUser.getName(),"Sergio");
+        
+    }
+    
+}
