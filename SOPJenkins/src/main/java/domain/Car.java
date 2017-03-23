3cc1d08abc3f6b38e540843fd8a553cfa5906b6d
fhict <fhict>
2017-03-23 14:22:13 +0100

    Delete get car passengers method

--- SOPJenkins/src/main/java/domain/Car.java
+++ SOPJenkins/src/main/java/domain/Car.java
@@ -13,9 +13,7 @@ public class Car {
 
     private int passengers;
 
-    public int getPassengers() {
-        return passengers;
-    }
+    
 
     public void setPassengers(int passengers) {
         this.passengers = passengers;
